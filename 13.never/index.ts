e118f04d712bd44c1a994db8cd950301337f5045
박혜민 <hyeminpak@gmail.com>
2023-06-25 16:02:16 +0900

    13.never 타입에 대해서 공부

---
+++ 13.never/index.ts
@@ -0,0 +1,14 @@
+{
+  //함수의 실행이 끝나지 않거나 , return 이 없어야 never 타입을 사용할수 있다.
+  function 함수(): never {
+    while (true) {
+      console.log('while 무한 반복');
+    }
+  }
+  function 에러함수(): never {
+    throw new Error('에러');
+  }
+
+  //배열의 타입을 정해주지 않으면 자동으로 never 타입으로 지정됨.
+  let array = [];
+}
